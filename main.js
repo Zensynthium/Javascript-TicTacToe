3cffda86ccdfc1f11f46bfdd8bb9f4693832fd54
Zensynthium <Anthony_m_h@outlook.com>
2020-05-18 20:30:09 -0400

    Added Draw statement using a counter method

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ tictactoe();
 
 var spaces = new Array(9);
 var turn = "O";
+var counter = 0;
 
 function mark(event) {
   var position = event.target.getAttribute("id");
@@ -16,7 +17,11 @@ function mark(event) {
     document.getElementById("turn-text").innerHTML = "Player One's Turn!";
     turn = "O";
   }
+
   document.getElementById(position).removeEventListener("click", mark);
+  counter++;
+
+
 
   if ((spaces[0] == spaces[1] && spaces[1] == spaces[2] && spaces[2] == "O") || (spaces[3] == spaces[4] && spaces[4] == spaces[5] && spaces[5] == "O") || (spaces[6] == spaces[7] && spaces[7] == spaces[8] && spaces[8] == "O") || (spaces[0] == spaces[3] && spaces[3] == spaces[6] && spaces[6] == "O") || (spaces[1] == spaces[4] && spaces[4] == spaces[7] && spaces[7] == "O") || (spaces[2] == spaces[5] && spaces[5] == spaces[8] && spaces[8] == "O") || (spaces[0] == spaces[4] && spaces[4] == spaces[8] && spaces[8] == "O") || (spaces[2] == spaces[4] && spaces[4] == spaces[6] && spaces[6] == "O")) {
     document.getElementById("win-text").innerHTML = "Player One Wins!";
@@ -31,16 +36,18 @@ function mark(event) {
       document.getElementById(i+1).removeEventListener("click", mark);
     }
   }
+
+  if (counter == 9 && document.getElementById("win-text").innerHTML == "") {
+    document.getElementById("win-text").innerHTML = "It's a Draw!";
+  }
 }
 
 function tictactoe() {
+counter = 0;
 document.getElementById("win-text").innerHTML = "";
 document.getElementById("turn-text").innerHTML = "Player One's Turn!";
-//9 spaces in a tic tac toe board && Array size of 9. Duhh.
 spaces = new Array(9);
-//How to see which players turn it is. Mark function toggles this like a switch
  turn = "O";
-//We'll be playing with click events so let's put them in
   for (var i = 0; i < 9; i++) {
     document.getElementById(i+1).addEventListener("click", mark);
     document.getElementById(i+1).innerHTML = "";
